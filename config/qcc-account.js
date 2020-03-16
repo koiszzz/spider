5f219d2d613e76abc3191d815799d1fc246ebc17
kois <koiszzz@users.noreply.github.com>
2020-03-16 09:17:40 +0800

    删除敏感信息

--- config/qcc-account.js
+++
@@ -1,5 +0,0 @@
-module.exports = [
-    // {"user": "13666999672", "password": "gyb111111"},
-    // {"user": "13559070456", "password": "lin774094"},
-    {"user": "15606038193", "password": "kFnUFnz5Ms9mh7R"},
-];
